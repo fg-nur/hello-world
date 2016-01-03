b0f263273b4723205d779549367daa997adb041b
fg-nur <fg.nur@outlook.com>
2016-01-03 03:02:07 +0000

    ADIVINALOL
    Tienes que adivinar el nombre del programa.
    
    Solo tienes 3 intentos.

---
+++ ADIVINALOL.py
@@ -0,0 +1,13 @@
+# ayyyyy
+# tienes que adivinar el nombre del programa, solo tines 3 intentos ayyy =D
+
+trys = 0
+
+while trys < 3:
+    name = input("queloque adiviname el nombre xDD ")
+    if name == "elmio":
+        print("AYYYYY LA PEGASTE XDD")
+        break
+    else:
+        print("sigue intentando XD")
+        trys = trys + 1
